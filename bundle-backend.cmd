e299a3d6ee21c43d944464449ea616185a399922
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-10-22 01:17:12 +0700

    Use bundled 7za.exe for packing backend

--- bundle-backend.cmd
+++ bundle-backend.cmd
@@ -3,4 +3,4 @@ del /s /q node_modules
 call npm install http://download.livereload.com/npm/livereload-0.2.0.tgz
 move node_modules/livereload backend
 del /q res/bundled/backend.7z
-"C:\Program Files\7-Zip\7z.exe" a res/bundled/backend.7z backend
+"%~dp0%res\7za.exe" a res/bundled/backend.7z backend
