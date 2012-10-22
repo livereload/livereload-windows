c789addda66af7da59c307fc1d5993b4b5b77a25
Andrey Tarantsov <andrey@tarantsov.com>
2012-10-22 13:38:44 +0200

    Bundle the backend without going through npm

--- bundle-backend.cmd
+++ bundle-backend.cmd
@@ -1,6 +1,12 @@
-del /s /q backend
-del /s /q node_modules
-call npm install http://download.livereload.com/npm/livereload-0.2.0.tgz
-move node_modules/livereload backend
-del /q res/bundled/backend.7z
+rd /s /q backend
+rd /s /q package
+del /q /f livereload-0.2.0.tar
+
+: "%~dp0res/curl" -O http://download.livereload.com/npm/livereload-0.2.0.tgz
+"%~dp0res/7za" x livereload-0.2.0.tgz
+"%~dp0res/7za" x livereload-0.2.0.tar
+del livereload-0.2.0.tar
+ren package backend
+
+del /q res\bundled\backend.7z
 "%~dp0%res\7za.exe" a res/bundled/backend.7z backend
