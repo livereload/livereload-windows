2ea2b4d61b701a19a912153cf64bb4e136c95005
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-10-24 04:02:40 +0700

    Handle 'status' message from node

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -110,6 +110,14 @@ namespace LiveReload
                 }
                 window.updateTreeView(projectsList);
             }
+            else if (messageType == "status")
+            {
+                var arg = (Dictionary<string, object>)b[1];
+
+                var status = (string)arg["status"];
+
+                window.textBlockStatus.Text = status;
+            }
             else if (messageType == "app.displayCriticalError")
             {
                 var arg = (Dictionary<string, object>) b[1];
