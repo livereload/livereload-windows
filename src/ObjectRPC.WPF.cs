838867fcd46fdaa675b01dbb2f2f40cfbf8fecf2
Andrey Tarantsov <andrey@tarantsov.com>
2012-10-30 22:41:42 +0100

    Add a simple TreeViewFacet

--- src/ObjectRPC.WPF.cs
+++ src/ObjectRPC.WPF.cs
@@ -22,11 +22,39 @@ namespace ObjectRPC.WPF
         }
     }
 
+    class TreeViewFacet : Facet<TreeView>
+    {
+        public TreeViewFacet(Entity entity, TreeView obj)
+            : base(entity, obj)
+        {
+        }
+
+        public IList<object> Data
+        {
+            set
+            {
+                var items = obj.Items;
+                items.Clear();
+                foreach (var itemDataRaw in value)
+                {
+                    var itemData = (Dictionary<string, object>)itemDataRaw;
+                    string id = (string)itemData["id"];
+                    string text = (string)itemData["text"];
+
+                    var tvi = new TreeViewItem();
+                    tvi.Header = text;
+                    items.Add(tvi);
+                }
+            }
+        }
+   }
+
     public static class UIFacets
     {
         public static void Register(RootEntity rpc)
         {
             rpc.Register(typeof(TextBlock), typeof(TextBlockFacet));
+            rpc.Register(typeof(TreeView), typeof(TreeViewFacet));
         }
     }
 }
